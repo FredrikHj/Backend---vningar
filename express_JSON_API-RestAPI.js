4db98ba732af5a1f1220a236f0e7c5c19cef23f2
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2019-05-22 22:53:06 +0200

    Mer

--- express_JSON_API-RestAPI.js
+++ express_JSON_API-RestAPI.js
@@ -1,22 +1,19 @@
 // Övning med routes     
 const express = require('express');
 const app = express();
+const fileSystem = require('fs');
 app.use(express.json());
 const port = 3000;
 
 let movieList = {data:
     [
-        {id: 0, name: "Die Hard", Rating: 3},
-        {id: 1, name: "Titanic", Rating: 5}, 
-        {id: 2, name: "The Rain", Rating: 4}
+        {id: 1, name: "Die Hard", rating: 3},
+        {id: 2, name: "Titanic", rating: 5}, 
+        {id: 3, name: "The Rain", rating: 4},
     ]
 };
-
 // Gets all movies
-app.get('/Movie', (req, res) => {
-    console.log(req.body);
-    
-    console.log(movieList);
+app.get('/Movie', (req, res) => {    
     res.send(movieList);
 });
 // Gets a specific movis
@@ -32,15 +29,43 @@ app.get('/Movie/:id', (req, res) => {
 
     res.send(sendItem);
 });
+
+
+// Create a new movie
+let countID = 0;
+function createID() {    
+    for (let index = 0; index < movieList.data.length; index++) {
+        const idMax = movieList.data[index];
+        countID = idMax.id;        
+    }
+    // Get the last id in my arr of movies
+    countID += 1;
+    console.log(countID);
+    return countID;
+}
 app.post('/Movie', (req, res) => {
+    console.log('Ny ' + createID());
+    let incommingPostData = {
+        id: createID(),
+        name: req.body.name,
+        rating: req.body.rating,
+    };    
+    movieList.data.push(incommingPostData);
+    // Save the movies in an json file
+    fileSystem.appendFile('express_JSON_API-RestAPI.json', JSON.stringify(incommingPostData), function(err) {
+        
+    })
+    
+    res.send(movieList);
+    
+});
+// Update a movie
+app.put('/Movie/:id', (req, res) => {
 
 });
-app.put('/Movie', (req, res) => {
+// Delete a movie
+app.delete('/Movie/:id', (req, res) => {
 
 });
-app.delete('/Movie', (req, res) => {
-
-});
-
 
  app.listen(port, () =>  console.log(`Example app listening on port ${port}!`));
