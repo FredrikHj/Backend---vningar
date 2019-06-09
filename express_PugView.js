52e64aa44602ca979357165f2174329be85be01f
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2019-06-09 11:45:43 +0200

    mer

--- express_PugView.js
+++ express_PugView.js
@@ -1,16 +1,25 @@
 // Övning med cookies
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use(express.json());
-app.use(urlencoded);
-
+//app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }
+        ));
 const port = 3000;
+let movie = '';
+//let listArr = ['efw'];
 
 app.set('Views', './Views');
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' });
+    res.render('index', { title: 'Filmer', headline: 'Filmer', lista: movie });
 });
+
+app.post('/', (req, res) => {
+    let movie = req.body.movies; 
+    //listArr.push(req.body.movies);    
+});
+
 
 app.listen(port, () =>  console.log(`Example app listening on port ${port}!`));
